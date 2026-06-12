0f50b343a6380a5978e62f1ae258c94f17ad6eaf
George Leyva <leyvageorge@hotmail.com>
2026-06-11 20:45:50 -0700

    Initial program setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/*
+* Download txt file
+* Append it to the file
+* Reversal method will reverse all the characters in txt file
+* Result will be saved on the same txt file.
+*/
+int main()
+{
+    //Open the text file named
+    //CSC450_CT5_mod.txt
+
+
+    return 0;
+}
