d6c87322cf7987c97e072f0ac4af82cf6d1229f6
George Leyva <leyvageorge@hotmail.com>
2026-06-11 21:52:01 -0700

    changes to json file to run properly

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 /*
 * Download txt file
 * Append it to the file
@@ -9,7 +12,20 @@ int main()
 {
     //Open the text file named
     //CSC450_CT5_mod.txt
+    std::cout << "Your program is looking in this folder:\n";
+    std::cout << std::filesystem::current_path() << std::endl;
+    std::cout << "------------------------------------------\n";
+    std::ofstream file("CSC450_CT5_mod5.txt", std::ios::app);
 
+    if (file.is_open()){
+        std::cout << "File is open" << std:: endl;
+        //Write the string to the file
+        file << "Welcome to the end of the line.";
+    } else {
+        std::cerr <<"Error: Could not open file" << std:: endl;
+    }
+    file.close();
+    std::cout << "Program ending." << std:: endl;
 
     return 0;
 }
